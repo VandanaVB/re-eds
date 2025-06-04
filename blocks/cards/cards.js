e4b99c61f015769ee272c61af22b26fc73c65b2c
VandanaVB <vandana.bv@axeno.co>
2025-06-04 18:29:29 +0530

    updated cards styling and script

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -12,6 +12,21 @@ export default function decorate(block) {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
+    const link = li.querySelector('a');
+    if (link) {
+      const url = link.getAttribute('href');
+      const wrapper = document.createElement('a');
+      wrapper.href = url;
+      wrapper.className = 'card-link';
+      moveInstrumentation(li, wrapper);
+
+      // Remove old links inside
+      li.querySelectorAll('a').forEach((a) => a.remove());
+
+      // Move all li children into wrapper
+      while (li.firstChild) wrapper.append(li.firstChild);
+      li.appendChild(wrapper);
+    }
     ul.append(li);
   });
   ul.querySelectorAll('picture > img').forEach((img) => {
