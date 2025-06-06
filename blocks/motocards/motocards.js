93e3dd2feb11303e23a304ee29a28d2f8ef05a2e
VandanaVB <158994302+VandanaVB@users.noreply.github.com>
2025-06-06 13:55:19 +0530

    Add moto cards block

---
+++ blocks/motocards/motocards.js
@@ -0,0 +1,39 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    moveInstrumentation(row, li);
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'motocards-card-image';
+      else div.className = 'motocards-card-body';
+    });
+    const link = li.querySelector('a');
+    if (link) {
+      const url = link.getAttribute('href');
+      const wrapper = document.createElement('a');
+      wrapper.href = url;
+      wrapper.className = 'motocards-link';
+      moveInstrumentation(li, wrapper);
+
+      // Remove old links inside
+      li.querySelectorAll('a').forEach((a) => a.remove());
+
+      // Move all li children into wrapper
+      while (li.firstChild) wrapper.append(li.firstChild);
+      li.appendChild(wrapper);
+    }
+    ul.append(li);
+  });
+  ul.querySelectorAll('picture > img').forEach((img) => {
+    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+    moveInstrumentation(img, optimizedPic.querySelector('img'));
+    img.closest('picture').replaceWith(optimizedPic);
+  });
+  block.textContent = '';
+  block.append(ul);
+}
