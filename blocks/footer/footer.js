c6e28dae622638043f6a649892397b287adf850d
Vibhanshu <vibhanshu.kislay@axeno.co>
2025-08-02 15:36:08 +0530

    Footer js changes

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -16,5 +16,40 @@ export default async function decorate(block) {
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
+  // --- Start of new logic ---
+  const container = footer.querySelector('.default-content-wrapper');
+  if (container) {
+    const allElements = [...container.children];
+    container.innerHTML = ''; // Clear the original container
+
+    const linksWrapper = document.createElement('div');
+    linksWrapper.classList.add('footer-links-wrapper');
+
+    const bottomWrapper = document.createElement('div');
+    bottomWrapper.classList.add('footer-bottom-wrapper');
+
+    let currentColumn;
+
+    allElements.forEach((el) => {
+      // If an H3 is found, start a new column
+      if (el.tagName === 'H3') {
+        currentColumn = document.createElement('div');
+        currentColumn.classList.add('footer-column');
+        currentColumn.append(el);
+        linksWrapper.append(currentColumn);
+      } else if (el.tagName === 'UL' && currentColumn) {
+        // If a UL follows an H3, add it to the current column
+        currentColumn.append(el);
+      } else {
+        // All other elements (copyright, legal links) go to the bottom
+        bottomWrapper.append(el);
+      }
+    });
+
+    container.append(linksWrapper);
+    container.append(bottomWrapper);
+  }
+  // --- End of new logic ---
+
   block.append(footer);
 }
